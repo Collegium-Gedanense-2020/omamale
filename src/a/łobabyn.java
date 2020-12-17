2fd1d9bd11d46a8a7f59a0aaf7e73c26ff7b2852
User <User@DESKTOP-8KI18M9>
2020-12-17 08:53:43 +0100

    rtwga

---
+++ src/a/łobabyn.java
@@ -0,0 +1,11 @@
+package a;
+import java.time.LocalDateTime;
+
+public class ³obabyn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Siema siema o tej porze ka¿dy wypiæ mo¿e:" + LocalDateTime.now());
+	}
+
+}
--- src/łobabyn.java
+++
@@ -1,9 +0,0 @@
-
-public class ³obabyn {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
